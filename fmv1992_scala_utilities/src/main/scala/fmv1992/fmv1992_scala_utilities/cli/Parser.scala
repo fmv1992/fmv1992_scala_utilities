6e8e0de8a30cf5a352ad894e2f529fa841641852
Felipe M. Vieira <fmv1992@gmail.com>
2019-06-18 20:50:05 -0300

    implemented some primitive operations for parsers

--- fmv1992_scala_utilities/src/main/scala/fmv1992/fmv1992_scala_utilities/cli/Parser.scala
+++ fmv1992_scala_utilities/src/main/scala/fmv1992/fmv1992_scala_utilities/cli/Parser.scala
@@ -40,6 +40,45 @@ object ParserTypes {
   type Parser = String ⇒ (String, OMS)
 }
 
+object PrimitiveParsers {
+
+  def newLine(s: String): (String, OMS) = {
+    val newlines = s.takeWhile(_ == '\n')
+    val rest = s.drop(newlines.size)
+    (rest, None)
+  }
+
+  def comment(s: String): (String, OMS) = {
+    val lines: List[String] = s.split("\n").toList
+    val commentLines = lines.takeWhile(_.startsWith("#"))
+    val otherLines = lines.drop(commentLines.length)
+    (otherLines.mkString("\n"), None)
+  }
+
+  def kvp(s: String): (String, OMS) = {
+    val lines: List[String] = s.split("\n").toList
+    val nonSpaceLines = lines.takeWhile(!_(0).isSpaceChar)
+    val otherLines = nonSpaceLines.drop(nonSpaceLines.length)
+    (otherLines.mkString("\n"), Some(Map.empty))
+  }
+
+}
+
+object CompoundedParsers {
+
+  val lines: Parser = CLIConfigParser.many1(PrimitiveParsers.newLine)
+
+  val comments: Parser = CLIConfigParser.many1(PrimitiveParsers.comment)
+
+  val content: Parser = CLIConfigParser.many1(PrimitiveParsers.kvp)
+
+  val orThree: Parser =
+    CLIConfigParser.or(lines, CLIConfigParser.or(comments, content))
+
+  val many1Three: Parser = CLIConfigParser.many1(orThree)
+
+}
+
 trait Parsers
 
 object Parser {}
@@ -50,7 +89,7 @@ object CLIConfigParser extends Parsers {
 
   // Parser combinators. --- {{{
 
-  lazy val Success: Parser = x ⇒ (x, Some(Map.empty))
+  // lazy val Success: Parser = x ⇒ (x, Some(Map.empty))
 
   def or(p1: Parser, p2: Parser): Parser = {
     (x: String) ⇒ {
@@ -73,7 +112,13 @@ object CLIConfigParser extends Parsers {
   }
 
   def many(p1: Parser): Parser = {
-    ???
+    (x: String) ⇒ {
+        val (s1: String, newP: OMS) = p1(x)
+        newP match {
+          case Some(_) ⇒ many(p1)(x)
+          case None ⇒ (s1, newP)
+        }
+      }
   }
 
   // --- }}}
@@ -81,6 +126,7 @@ object CLIConfigParser extends Parsers {
   case object Newline extends CLIConfigParser {
     val get = "\n"
   }
+
   case class Comment(get: OMS) extends CLIConfigParser
   case class Config(get: OMS) extends CLIConfigParser
   case class SubConfig(get: OMS) extends CLIConfigParser
