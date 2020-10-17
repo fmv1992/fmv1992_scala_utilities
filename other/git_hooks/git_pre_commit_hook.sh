ee1dc3a3c61709c8cdf4ddfa61e640e2a1629dd0
Felipe M. Vieira <fmv1992@gmail.com>
2020-10-17 20:54:41 -0300

    do not recompile everything...

--- other/git_hooks/git_pre_commit_hook.sh
+++ other/git_hooks/git_pre_commit_hook.sh
@@ -7,19 +7,12 @@ export MAKEFLAGS='-j1'
 # It should be symlinked to ../../.git/hooks/pre-commit.
 
 # Halt on error.
-set -e
-set -x
+set -euo pipefail
 
 # Go to execution directory.
 # cd $(dirname $0)
 
-git diff --name-only --cached --diff-filter=ACMRTUXB \
-    | grep '\.scala' \
-    | parallel \
-        -I % \
-        --verbose \
-        --jobs $((2*$(nproc))) \
-        "vim -i NONE -n -c 'VimScalafmt' -c 'noautocmd x!' %"
+make format
 git diff --name-only --cached --diff-filter=ACMRTUXB \
     | xargs --verbose git add --force
 # --diff-filter=ACDMRTUXB
@@ -31,25 +24,24 @@ test -f $versionfile
 
 make --jobs 1 dev
 make --jobs 1 clean
-make --jobs 1 assembly
 make --jobs 1 test
 
-# Bump minor version.
-fileversion=$(find $versionfile -name 'version' -type f)
-
-# ???: Do not bump if version file is already added:
-# git diff --name-only --cached --diff-filter=AM
-tmpversion=$(mktemp)
-if verify_is_backwards_compatible.sh "$PWD" 0
-then
-    # Bump patch version.
-    cat "${fileversion}" | python3 -c "import sys ; i = sys.stdin.read(); j = i.split('.') ; j[2] = str(int(j[2]) + 1) ; print('.'.join(j), end='')" > "$tmpversion"
-else
-    # Bump minor and reset patch.
-    cat "${fileversion}" | python3 -c "import sys ; i = sys.stdin.read(); j = i.split('.') ; j[-1] = '0'; j[1] = str(int(j[1]) + 1) ; print('.'.join(j), end='')" > "$tmpversion"
-fi
-mv "$tmpversion" "${fileversion}"
-git add -f "$fileversion"
+# # Bump minor version.
+# fileversion=$(find $versionfile -name 'version' -type f)
+#
+# # ???: Do not bump if version file is already added:
+# # git diff --name-only --cached --diff-filter=AM
+# tmpversion=$(mktemp)
+# if verify_is_backwards_compatible.sh "$PWD" 0
+# then
+#     # Bump patch version.
+#     cat "${fileversion}" | python3 -c "import sys ; i = sys.stdin.read(); j = i.split('.') ; j[2] = str(int(j[2]) + 1) ; print('.'.join(j), end='')" > "$tmpversion"
+# else
+#     # Bump minor and reset patch.
+#     cat "${fileversion}" | python3 -c "import sys ; i = sys.stdin.read(); j = i.split('.') ; j[-1] = '0'; j[1] = str(int(j[1]) + 1) ; print('.'.join(j), end='')" > "$tmpversion"
+# fi
+# mv "$tmpversion" "${fileversion}"
+# git add -f "$fileversion"
 
 # `verify_is_backwards_compatible.sh` was once displayed here.
 
